b0d4e572fac9d3d59f453dc3a4a6d255c8a1a361
Affomadou <amadouaffo@gmail.com>
2022-03-17 20:33:55 +0100

    Starterkit

---
+++ src/pages/about/about.js
@@ -0,0 +1,14 @@
+import "./about.scss";
+
+
+// import { useRef, useState } from "react";
+
+
+const About = () => {
+    return (
+        <section className='about' >
+            Hola my about
+        </section>
+    );
+}
+export default About;
---
+++ src/pages/home/home.js
@@ -0,0 +1,14 @@
+import "./home.scss";
+
+
+// import { useRef, useState } from "react";
+
+
+const Home = () => {
+    return (
+        <section className='home' >
+            Hola my starter
+        </section>
+    );
+}
+export default Home;
