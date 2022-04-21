9836c472017ecb5744a0b68308e85958d15623e4
Affomadou <amadouaffo@gmail.com>
2022-04-21 21:41:18 +0200

    h

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { useLocation, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/home';
 import About from './pages/about/about';
 function App() {
+  const location = useLocation();
+  const imageDetails = {
+    width: 262,
+    height: 325,
+  };
   return (
-    <div className="App">
-
-
-      <Routes>
-        <Route path="/" element={<Home />} ></Route>
-        <Route path="/about" element={<About />} ></Route>
-
-      </Routes>
-
+    <div className="App" >
+      <>
+        <AnimatePresence exitBeforeEnter >
+          <Routes key={location.pathname} location={location}>
+            <Route path="/" element={<Home imageDetails={imageDetails} />} ></Route>
+            <Route path="/about" element={<About imageDetails={imageDetails} />} ></Route>
+          </Routes>
+        </AnimatePresence>
+      </>
     </div>
   );
 }
 
 export default App;
+
+
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,14 +1,61 @@
 import "./home.scss";
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import ProgressiveImage from "react-progressive-image";
 
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
 
 // import { useRef, useState } from "react";
 
 
-const Home = () => {
-    return (
-        <section className='home' >
-            Hola my starter
-        </section>
-    );
-}
+const Home = ({ imageDetails, image }) => (
+    <>
+        <main>
+            <div className='container'>
+                <div className='row center'>
+                    <div className='image-container'>
+                        <div
+                            className='thumbnail'
+                            ref={image}
+                            style={{
+                                width: imageDetails.width,
+                                height: imageDetails.height,
+                            }}>
+
+                            <div className='frame'>
+                                <Link to={`/about`}>
+                                    <ProgressiveImage
+                                    src={require("../../images/g.webp")}
+                                    placeholder={require("../../images/g.jpg")}
+                                    >
+                                        {(src) => (
+                                            <motion.img
+                                                src={src}
+                                                alt='Yasmeen Tariq'
+                                                whileHover={{ scale: 1.1 }}
+                                                transition={transition}
+                                            />
+                                        )}
+                                    </ProgressiveImage>
+                                </Link>
+                            </div>
+                        </div>
+                        <motion.div
+                            exit={{ opacity: 0 }}
+                            transition={transition}
+                            className='information'>
+                            <div className='title'>IRIINA</div>
+                            <div className='location'>
+                                <span>28.538336</span>
+                                <span>-81.379234</span>
+                            </div>
+                        </motion.div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </>
+);
+
 export default Home;
